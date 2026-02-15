5711c3c3f13be4123e19e47c5e0ed81c5215daa9
Josh Nussbaum <joshnuss@gmail.com>
2026-02-15 18:20:15 -0500

    Adds sign in page

---
+++ src/routes/sign-in/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { auth } from '$lib/auth-client'
+
+  let email = $state('')
+  let password = $state('')
+
+  async function onsubmit(event: Event) {
+    event.preventDefault()
+
+    const result = await auth.signIn.email({
+      email,
+      password
+    })
+
+    console.log({ result })
+  }
+</script>
+
+<h1>Sign in</h1>
+
+<form {onsubmit}>
+  <input type="email" required bind:value={email} placeholder="E-mail">
+  <input type="password" required bind:value={password} placeholder="Password"/>
+  <button>Sign in</button>
+</form>
