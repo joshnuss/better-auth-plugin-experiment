4d93204386cc2e3a9dabb3780899f53b98c541fb
Josh Nussbaum <joshnuss@gmail.com>
2026-02-15 18:19:52 -0500

    Add hook and endpoint

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -5,7 +5,10 @@ import { PUBLIC_ORIGIN } from '$env/static/public'
 import { getRequestEvent } from '$app/server'
 import { sveltekitCookies } from 'better-auth/svelte-kit'
 
-import type { BetterAuthPlugin } from "better-auth";
+import type { BetterAuthPlugin } from "better-auth"
+import { APIError, sessionMiddleware, createAuthMiddleware } from "better-auth/api"
+import { createAuthEndpoint } from 'better-auth/plugins'
+import { z } from 'zod'
 
 export const myPlugin = () =>
   ({
@@ -17,8 +20,29 @@ export const myPlugin = () =>
             type: 'string',
             required: true,
           }
-        }
+        },
       }
+    },
+    hooks: {
+      after: [{
+        matcher: (ctx) => ctx.path == '/sign-in/email',
+        handler: createAuthMiddleware(async (ctx) => {
+          throw new APIError('OOPS')
+          return ctx.json({ test: ctx.path })
+        })
+      }]
+    },
+    $ERROR_CODES: {
+      OOPS: 'Holy cow'
+    },
+    endpoints: {
+      example22: createAuthEndpoint('/foo/bar', {
+        method: 'GET',
+        //use: [ sessionMiddleware ],
+          query: z.object({ name2: z.string().optional() }),
+      }, async (ctx) => {
+        return ctx.json({ message: 'yo' })
+      })
     }
   } satisfies BetterAuthPlugin)
 
@@ -28,11 +52,19 @@ export const auth = betterAuth({
   database: prismaAdapter(db, {
     provider: 'postgresql'
   }),
+  emailAndPassword: {
+    enabled: true
+  },
   plugins: [
     myPlugin(),
     sveltekitCookies(getRequestEvent) // must be last
   ]
 })
+
+// auth.api.example22({
+//   query: {
+//   }
+// })
 
 export type Session = (typeof auth.$Infer.Session)['session']
 export type User = (typeof auth.$Infer.Session)['user']
