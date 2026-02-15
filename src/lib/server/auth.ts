4b18e26686ebfcb273e0eaaeb06bf4e91f262ff4
Josh Nussbaum <joshnuss@gmail.com>
2026-02-14 22:16:23 -0500

    Adds city field

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -5,12 +5,31 @@ import { PUBLIC_ORIGIN } from '$env/static/public'
 import { getRequestEvent } from '$app/server'
 import { sveltekitCookies } from 'better-auth/svelte-kit'
 
+import type { BetterAuthPlugin } from "better-auth";
+
+export const myPlugin = () =>
+  ({
+    id: "myPlugin",
+    schema: {
+      user: {
+        fields: {
+          city: {
+            type: 'string',
+            required: true,
+          }
+        }
+      }
+    }
+  } satisfies BetterAuthPlugin)
+
+
 export const auth = betterAuth({
   baseURL: PUBLIC_ORIGIN,
   database: prismaAdapter(db, {
     provider: 'postgresql'
   }),
   plugins: [
+    myPlugin(),
     sveltekitCookies(getRequestEvent) // must be last
   ]
 })
